73efb5a0d9438ab7f7f5fa53a3564db16e4df580
99002509 <hridya.m@ltts.com>
2021-06-09 11:39:48 +0530

    Pushing code

--- ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetCoffeeShop.cs
@@ -8,6 +8,32 @@ namespace ProblemThree_CompleteSolution_BL
 {
     public class GourmetCoffeeShop : GourmetShop, ITaxCompute
     {
-        
+        public double TotalBillValue
+        {
+            get;
+            set;
+        }
+        public GourmetCoffeeShop(int iCode, string iName, Dictionary<int,double> iPrice)
+        {
+            this.ItemCode = iCode;
+            this.ItemName = iName;
+            this.ItemPrice = iPrice;
+        }
+        public override double PayPerPiece(int quantity)
+        {
+            TotalBillValue = quantity * this.ItemPrice[this.ItemCode];
+            return TotalBillValue;
+        }
+        public override double PayPerWeight(int weight)
+        {
+            TotalBillValue = weight * this.ItemPrice[this.ItemCode];
+            return TotalBillValue;
+
+        }
+
+        public double TaxOnTotalBill()
+        {
+            return 0.05 * TotalBillValue;
+        }
     }
 }
--- ProblemThree_CompleteSolution_BL/GourmetShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetShop.cs
@@ -7,9 +7,33 @@ using System.Threading.Tasks;
 namespace ProblemThree_CompleteSolution_BL
 {
     public abstract class GourmetShop
-    {          
+    {
+        
+        public int ItemCode
+        {
+            get { return this.ItemCode; }
+            set { this.ItemCode = value; }
+        }
+
+        public string ItemName
+        {
+            get { return this.ItemName; }
+            set { this.ItemName = value; }
+        }
+
+        public Dictionary<int,double> ItemPrice
+        {
+            get { return this.ItemPrice; }
+            set { this.ItemPrice = value; }
+        }
+        
         public abstract double PayPerPiece(int quantity);
         public abstract double PayPerWeight(int weight);
+        public GourmetShop(int iCode, string iName)
+        {
+            ItemCode = iCode;
+            ItemName = iName;
+        }
 
     }
 }
